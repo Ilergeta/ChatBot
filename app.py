26c38a9e53d942e117fa07bb728bcaf4eb4d9f24
Ilergeta <josepespa@hotmail.com>
2021-11-04 20:35:55 +0100

    First app_2

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def main():
     st.markdown('<h5 style="font-family:Courier;text-align:center;">'+input_text+'</h5>',
             unsafe_allow_html=True,)
 
-    test_questions = input_text
+    test_questions = [input_text]
 
     # Load module containing USE
     embed = hub.load('https://tfhub.dev/google/universal-sentence-encoder/4')
