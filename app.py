0fd7b02be3262f38e9ea7396fc59d96873297607
Ilergeta <josepespa@hotmail.com>
2021-11-04 09:48:02 +0100

    First app_2

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ Here's our first attempt at using data to create a table:
 import pandas as pd
 df = pd.DataFrame({
   'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
+  'second column': [10, 20, 50, 40]
 })
 
 df
