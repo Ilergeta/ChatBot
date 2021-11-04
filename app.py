4412cc0a85954c8093ff12167644bdabc4eeb120
Ilergeta <josepespa@hotmail.com>
2021-11-04 09:45:42 +0100

    First app

---
+++ app.py
@@ -0,0 +1,12 @@
+"""
+# My first app
+Here's our first attempt at using data to create a table:
+"""
+
+import pandas as pd
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+df
