ebc1e99fa26fe2a0c558b659fd760aa8940cfb3f
Ilergeta <josepespa@hotmail.com>
2021-11-04 16:23:58 +0100

    First app_2

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ import pandas as pd
 import streamlit as st
 import numpy as np
 
+df = pd.DataFrame({'first column': [1, 2, 3], 'second column': [4, 5, 6]})
+
 option = st.selectbox(
     'Which number do you like best?',
      df['first column'])
