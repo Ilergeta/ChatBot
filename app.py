7705c9fa74b3c6a136dd9863a96c741e5382c1d0
Ilergeta <josepespa@hotmail.com>
2021-11-04 10:10:13 +0100

    First app_2

--- app.py
+++ app.py
@@ -7,5 +7,8 @@ import pandas as pd
 import streamlit as st
 import numpy as np
 
-dataframe = np.random.randn(10, 20)
-st.write(dataframe)
+dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
+
+st.dataframe(dataframe.style.highlight_max(axis=0))
