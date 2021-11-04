755da3570dac2393f51ec4333ff834043ed9f366
Ilergeta <josepespa@hotmail.com>
2021-11-04 16:21:13 +0100

    First app_2

--- app.py
+++ app.py
@@ -7,7 +7,9 @@ import pandas as pd
 import streamlit as st
 import numpy as np
 
-st.text_input("Your name", key="name")
+if st.checkbox('Show dataframe'):
+    chart_data = pd.DataFrame(
+       np.random.randn(20, 3),
+       columns=['a', 'b', 'c'])
 
-# You can access the value at any point with:
-st.session_state.name
+    chart_data
