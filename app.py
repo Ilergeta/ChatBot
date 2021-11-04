ba84d064c68521829a476614276c20d199d4f2db
Ilergeta <josepespa@hotmail.com>
2021-11-04 17:05:29 +0100

    First app_2

--- app.py
+++ app.py
@@ -10,21 +10,31 @@ import time
 
 text_short = """
 __ITC ML similarity model__:
-_This website is for entertainment purposes only!_
+
+
+_This website is not for legal purposes and it is work in progress!_
+
+
 This website uses a machine learning model to generate trade policies based on
 previous ITC policies official documents made for similar trade issues detected
 """
 
 text_long = """
 __ITC ML similarity model__:
-_This website is for entertainment purposes only!_
+
+
+_This website is not for legal purposes and it is work in progress!_
+
+
 This website uses a machine learning model to generate trade policies based on
 previous ITC policies official documents made for similar trade issues detected.
 
 __Description__:
-This project uses a USE (Universal Sentence Encoder) published by Google at TensorFlow
-Hub and it is based on a DAN (Deep Averaging Network) architecture. Once new input text
-is encoded it is compared (with cosinus similarity) to previous ITC policies and
+
+
+This project uses a USE (Universal Sentence Encoder) published by Google at [TensorFlow
+Hub](https://tfhub.dev/google/universal-sentence-encoder/4) and it is based on a DAN (Deep Averaging Network) architecture. Once new input text
+is encoded it is compared (with [cosine similarity](https://en.wikipedia.org/wiki/Cosine_similarity) ) to previous ITC policies and
 model outputs more similar activities proposed for those issues.
 """
 
