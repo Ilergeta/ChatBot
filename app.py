413156cae5d6d408a58de9c5679ecb50cb2a9118
Ilergeta <josepespa@hotmail.com>
2021-11-04 10:11:19 +0100

    First app_2

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ import pandas as pd
 import streamlit as st
 import numpy as np
 
-dataframe = pd.DataFrame(
-    np.random.randn(10, 20),
-    columns=('col %d' % i for i in range(20)))
-st.table(dataframe)
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
