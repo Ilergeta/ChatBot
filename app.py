0bfc54d013aa46952d37f19ad90d6c9b3b75e894
Ilergeta <josepespa@hotmail.com>
2021-11-04 10:12:01 +0100

    First app_2

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ import pandas as pd
 import streamlit as st
 import numpy as np
 
-chart_data = pd.DataFrame(
-     np.random.randn(20, 3),
-     columns=['a', 'b', 'c'])
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
 
-st.line_chart(chart_data)
+st.map(map_data)
