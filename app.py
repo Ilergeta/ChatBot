c4cf247544ef8d9f2efc8b79b431697f31033e93
Ilergeta <josepespa@hotmail.com>
2021-11-04 16:28:16 +0100

    First app_2

--- app.py
+++ app.py
@@ -6,14 +6,18 @@ Here's our first attempt at using data to create a table:
 import pandas as pd
 import streamlit as st
 import numpy as np
+import time
 
-left_column, right_column = st.columns(2)
-# You can use a column just like st.sidebar:
-left_column.button('Press me!')
+'Starting a long computation...'
 
-# Or even better, call Streamlit functions inside a "with" block:
-with right_column:
-    chosen = st.radio(
-        'Sorting hat',
-        ("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"))
-    st.write(f"You are in {chosen} house!")
+# Add a placeholder
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+  # Update the progress bar with each iteration.
+  latest_iteration.text(f'Iteration {i+1}')
+  bar.progress(i + 1)
+  time.sleep(0.1)
+
+'...and now we\'re done!'
