3fa8d20f09472f85aff3c77de3ede80c6016aef4
Ilergeta <josepespa@hotmail.com>
2021-11-04 10:10:54 +0100

    First app_2

--- app.py
+++ app.py
@@ -10,5 +10,4 @@ import numpy as np
 dataframe = pd.DataFrame(
     np.random.randn(10, 20),
     columns=('col %d' % i for i in range(20)))
-
-st.dataframe(dataframe.style.highlight_max(axis=0))
+st.table(dataframe)
