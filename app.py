51716cad315c93058f9e1c024683860a369ada23
Ilergeta <josepespa@hotmail.com>
2021-11-08 12:55:31 +0100

    Add code comments

--- app.py
+++ app.py
@@ -1,65 +1,97 @@
-"""
-# My first app
-Here's our first attempt at using data to create a table:
-"""
 import pathlib
 
-import os
-import pandas as pd
 import streamlit as st
 import numpy as np
 import pickle
 import matplotlib.pyplot as plt
 from SessionState import _get_state
-import time
 
-import tensorflow as tf
+
 import tensorflow_hub as hub
+
 
 def main():
 
+    # Needed to clean text_area st object
     state = _get_state()
 
+    # Add web page title
     st.set_page_config(page_title="ITC ML ChatBot")
 
+    # Load model (as st.cache, so it will load only first time execution)
+    # and get variables that remain constant for every new query
     embed, unique_questions, question_orig_encodings, lucky_questions, data_pd = load_model()
 
+    # Load first section page and get text filled by user
     input_text = load_page(state, lucky_questions)
 
+    # Convert string text into list to make compatible with existing algorithm
     test_questions = [input_text]
 
+    # Once new text is input new calculations are made
     if test_questions[0] != '':
         # Create encodings for test questions
         question_encodings = embed(test_questions)
 
+        # Make inner product between new question and previous ones (all word embeddings are unitary)
         use_result = np.inner(question_encodings, question_orig_encodings)
 
+        # Calculate maximum cosine similarity
         best_value = np.argmax(use_result[0])
 
+        # Load first section answer (Introduction and tag outputs)
         make_response(best_value)
 
+        # From similar questions obtained, get their activities as proposed policies
         response_list = data_pd[data_pd['Issue'].str.contains(unique_questions[best_value],
                                                               regex=False)]['Activity'].tolist()
 
-        #st.markdown('---')
-
+        # Show proposed policies
         for i, response_text in enumerate(response_list):
             st.markdown('*Policy {}*:'.format(i+1))
             st.markdown(response_text)
-            #st.markdown('---')
 
+        # Define viz button logic
         if st.button('Show similarity viz'):
             make_viz(best_value, use_result)
 
             if st.button('Close similarity viz'):
                 st.pyplot()
 
+    # Load last section page
     load_footer()
 
+    # Need as last order for well working reset button
     state.sync()
 
 
+@st.cache
+def load_model():
+    """
+    Load model (as st.cache, so it will load only first time execution)
+    and get variables that remain constant for every new query
+
+    Those variables need to be previously computed and saved in a pickle file
+    in order to minimize calculations in our web
+    """
+    # Load module containing USE
+    embed = hub.load('https://tfhub.dev/google/universal-sentence-encoder/4')
+
+    # Load previous data from pickle
+    pickle_name = 'USE_inputs_2021-11-04_204358.bak'
+    with open('./data/' + pickle_name, 'rb') as file_open:
+        unique_questions, question_orig_encodings, lucky_questions, data_pd, _ = pickle.load(file_open)
+
+    return embed, unique_questions, question_orig_encodings, lucky_questions, data_pd
+
+
 def load_page(state, lucky_questions):
+    """
+    Load first section page: sidebar text, text_area (with titles) and reset+random buttons.
+
+    Return text inside text_are as string
+    """
+
     text_long = """
     __ITC ML similarity model__:
     
@@ -79,11 +111,14 @@ def load_page(state, lucky_questions):
     model outputs more similar activities proposed for those issues.
     """
 
+    # Show ITC logo from wikipedia
     st.markdown('<img style="float: left; width:20%; heigh:auto" src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/International_Trade_Centre_Logo.svg/1200px-International_Trade_Centre_Logo.svg.png" />',
                 unsafe_allow_html=True)
 
+    # Load sidebar message
     st.sidebar.markdown(text_long)
 
+    # Show text_area with title and user information
     st.markdown('<h2 style="font-family:Arial;text-align:center;">ITC ML TradeBot</h2>',
             unsafe_allow_html=True,)
 
@@ -97,29 +132,31 @@ def load_page(state, lucky_questions):
       max_chars=5000
     )
 
+    # Define two page columns in order to show both buttons at same line
     left_column, right_column = st.beta_columns(2)
 
+    # Define Reset button logic
     with left_column:
         if st.button('Clear Text'):
             state.input = ''
 
+    # Define random text button logic
     with right_column:
         if st.button("I'm Feeling Lucky"):
             state.input = lucky_questions[np.random.randint(len(lucky_questions))]
 
     return state.input
 
-@st.cache
-def load_model():
-    # Load module containing USE
-    embed = hub.load('https://tfhub.dev/google/universal-sentence-encoder/4')
-    # Load previous data from pickle
-    pickle_name = 'USE_inputs_2021-11-04_204358.bak'
-    with open('./data/' + pickle_name, 'rb') as file_open:
-        unique_questions, question_orig_encodings, lucky_questions, data_pd, _ = pickle.load(file_open)
-    return embed, unique_questions, question_orig_encodings, lucky_questions, data_pd
 
 def make_response(best_value):
+    """
+    -----------
+    TODO
+    Need to implement tags outputs
+    -----------
+    Load first section answer (Introduction and tag outputs) and show it
+    """
+
     response_text = """
     It seems you have a **<best_issue_tag>** issue. From our experience, we recommend to you to 
     take some **<best_activity_tag>** policy.
@@ -127,9 +164,14 @@ def make_response(best_value):
     Above we show you some detailed policies that can fit to your issue.    
     """
 
+    # Show first section answer
     st.markdown(response_text)
 
+
 def make_viz(best_value, use_result):
+    """
+    Make cosine similarity visualization marking similar question obtained
+    """
     # Make visualization
     fig, ax = plt.subplots(figsize=(10, 8))
     ax.set_title('Similarity values for USE model', fontsize=18)
@@ -142,7 +184,12 @@ def make_viz(best_value, use_result):
             color='darkorange', weight='bold')
     st.pyplot(fig)
 
+
 def load_footer():
+    """
+    Load last section page
+    """
+
     text_short = """
     __Disclamer__:
 
@@ -154,6 +201,7 @@ def load_footer():
     previous ITC policies official documents made for similar trade issues detected
     """
 
+    # Show footer section with markdown separator
     st.markdown('---')
     st.markdown(text_short)
 
